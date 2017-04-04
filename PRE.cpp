4d1a36a74719e166ac08b6212d14a41229a49db9
Yiyi Wang <shd101wyy@sina.com>
2017-04-04 16:32:37 -0500

    done Safe and Earliest.

--- PRE.cpp
+++ PRE.cpp
@@ -2,6 +2,8 @@
 
 #define DEBUG_TYPE "pre"
 #include "llvm/Transforms/Scalar.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
@@ -15,6 +17,7 @@
 #include <vector>
 #include <set>
 #include <unordered_set>
+#include <map>
 #include <algorithm>
 using namespace llvm;
 using namespace std;
@@ -41,12 +44,17 @@ namespace {
     // Entry point for the overall pre pass
     bool runOnFunction(Function &F);
 
+    std::map<Instruction*, bool> mem_dsafe;
+    std::map<Instruction*, bool> mem_earliest;
+
     std::set<term_t> getPartialRedundantExpressions(Function &F);
     bool Used(Instruction &inst, term_t term);
     bool Transp(Instruction &inst, term_t term);
     bool DSafe(Instruction &inst, term_t term);
+    bool Earliest(Instruction &inst, term_t term);
     Instruction* getBinarySuccessor(Instruction *inst);
     std::set<Instruction*> getSuccessors(Instruction *inst);
+    std::set<Instruction*> getPredecessors(Instruction *inst);
 
     // getAnalysisUsage - List passes required by this pass.  We also know it
     // will not alter the CFG, so say so.
@@ -126,15 +134,59 @@ bool PRE::Transp(Instruction &inst, term_t term) {
 }
 
 bool PRE::DSafe(Instruction &inst, term_t term) {
-  if (Used(inst, term)) return true;
-  if (Transp(inst, term)) {
+  if (mem_dsafe[&inst]) return mem_dsafe[&inst];
+
+  BasicBlock *bb = inst.getParent();
+  Function *f = bb->getParent();
+
+  bool dsafe = false;
+  if (bb == &(f->back()) && (&inst) == &(bb->back()))  {  // if n == e
+    dsafe = false;
+  } else if (Used(inst, term)) {
+    dsafe = true;
+  } else if (Transp(inst, term)) {
+    dsafe = true;
     std::set<Instruction*> successors = getSuccessors(&inst);
     for (auto I = successors.begin(), E = successors.end(); I != E; ++I) {
-      bool dsafe = DSafe(&*I, term); // TODO: memoization
-      if (!dsafe) return false;
+      if (!DSafe(**I, term)) {
+        dsafe = false;
+        break;
+      }
+    }
+  } else {
+    dsafe = false;
+  }
+
+  mem_dsafe[&inst] = dsafe;
+  return dsafe;
+}
+
+bool PRE::Earliest(Instruction &inst, term_t term) {
+  if (mem_earliest[&inst]) return mem_earliest[&inst];
+
+  BasicBlock *bb = inst.getParent();
+  Function *f = bb->getParent();
+
+  bool earliest = false;
+  if (&(f->front()) == bb && &(bb->front()) == &inst) { // if n == s
+    earliest = true;
+  } else {
+    std::set<Instruction*> predecessors = getPredecessors(&inst);
+    earliest = false;
+    for (auto I = predecessors.begin(), E = predecessors.end(); I != E; ++I) {
+      Instruction *m = *I;
+      if (!Transp(*m, term)) {
+        earliest = true;
+        break;
+      } else if (!DSafe(*m, term) && Earliest(*m, term)) {
+        earliest = true;
+        break;
+      }
     }
   }
-  return false;
+
+  mem_earliest[&inst] = earliest;
+  return earliest;
 }
 
 Instruction* PRE::getBinarySuccessor(Instruction *inst) {
@@ -150,33 +202,62 @@ Instruction* PRE::getBinarySuccessor(Instruction *inst) {
       return i;
     }
   }
-  return null; // no successor found
+  return NULL; // no successor found
 }
 
 
 std::set<Instruction*> PRE::getSuccessors(Instruction *inst) {
-  std::set<Instruction*> successors;
+  std::set<Instruction*> successorsSet;
 
   BasicBlock *parent = inst->getParent();
-  if (&*(--(parent->end())) == inst) {   // the end of basic block
+  if (&(parent->back()) == inst) {   // the end of basic block
+    /*
     TerminatorInst *termInst = parent->getTerminator();
     unsigned numSuccessors = termInst->getNumSuccessors();
     for (unsigned i = 0; i < numSuccessors; i++) {
       BasicBlock *successorBB = termInst->getSuccessor(i);
       Instruction *firstInst = &*(successorBB->begin());
-      successors.insert(firstInst);
+      successorsSet.insert(firstInst);
     }
-  } else { // has multiple successors
+    */
+    for (auto it : successors(parent)) {
+      BasicBlock* bb = &*it;
+      successorsSet.insert(&(bb->front()));
+    }
+
+  } else { // has single successor
     for (auto I = parent->begin(), E = parent->end(); I != E; ++I) {
       if (inst == &*I) {
         I++;
-        successors.insert(&*I);
+        successorsSet.insert(&*I);
         break;
       }
     }
   }
 
-  return successors;
+  return successorsSet;
+}
+
+std::set<Instruction*> PRE::getPredecessors(Instruction *inst) {
+  std::set<Instruction*> predecessorsSet;
+
+  BasicBlock *parent = inst->getParent();
+  if (&(parent->front()) == inst) {   // the begin of basic block
+    for (auto it : predecessors(parent)) {
+      BasicBlock* bb = &*it;
+      predecessorsSet.insert(&(bb->back()));
+    }
+  } else { // has single successor
+    for (auto I = parent->begin(), E = parent->end(); I != E; ++I) {
+      if (inst == &*I) {
+        I--;
+        predecessorsSet.insert(&*I);
+        break;
+      }
+    }
+  }
+
+  return predecessorsSet;
 }
 
 
