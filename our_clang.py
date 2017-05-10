f057de7e861e0ead842cd929d6a8ff0fab85ce41
Tarun Rajendran <tarunrajendran@gmail.com>
2017-05-09 22:25:37 -0500

    Editing clang wrapper

--- our_clang.py
+++ our_clang.py
@@ -19,9 +19,10 @@ while i < len(argv):
     if argv[i] == '-o':
         i += 1
         output_file = argv[i]
-    elif argv[i][0] != '-' and (argv[i].endswith('.c') or argv[i].endswith('.cpp') or (argv[i].endswith('.o'))):
+    elif argv[i][0] != '-' and (argv[i].endswith('.c') or argv[i].endswith('.o')):
         input_file = argv[i]
     i += 1
+
 
 o_file = output_file
 if not (o_file.endswith('.o')):
@@ -31,8 +32,12 @@ if not (o_file.endswith('.o')):
 #print(output_file)
 #print(o_file)
 
-if not (input_file.endswith('.o')):
-    os.system('clang -c -O0 -emit-llvm ' + input_file + ' -o - | opt -stats -load /Users/wangyiyi/百度云同步盘/Github/CS426_Unit_Project/llvm/build/lib/PREviaLCM.dylib -pre | llc -filetype=obj > ' + o_file)
-
-if not (output_file.endswith('.o')):
-    os.system('clang -g ' + o_file + ' -o ' + output_file)
+# .c -> .o
+if (input_file.endswith('.c')):
+    os.system('clang -c -O0 -emit-llvm ' + input_file + ' -o - | /Users/tarunraj/Projects/build/bin/opt -stats -load /Users/tarunraj/Projects/build/lib/PREviaLCM.dylib -reassociate -newgvn | /Users/tarunraj/Projects/build/bin/llc -filetype=obj > ' + output_file)
+    if not (output_file.endswith('.o')):
+        os.system('clang ' + o_file + ' -o ' + output_file)
+elif (len(output_file) > 0):  # .o -> exe
+    os.system('clang ' + input_file + ' -o ' + output_file)
+else: # else
+    os.system('clang ' + argv)
