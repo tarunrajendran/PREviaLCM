84622458aee202a3916601fbdf68ce883b344127
Tarun Rajendran <tarunrajendran@gmail.com>
2017-05-10 00:03:04 -0500

    Pushing updated clang

--- our_clang.py
+++ our_clang.py
@@ -9,20 +9,25 @@ if len(sys.argv) == 1:
 ## print will cause lnt error. so don't print.
 
 argv = sys.argv[1:]
+new_argv = []
 #print(argv)
 
 input_file = ""
 output_file = ""
+include_line = ""
 
 i = 0
 while i < len(argv):
     if argv[i] == '-o':
-        i += 1
+        i += 1;
         output_file = argv[i]
-    elif argv[i][0] != '-' and (argv[i].endswith('.c') or argv[i].endswith('.o')):
+    elif (argv[i].endswith('.c') or argv[i].endswith('.o')):
         input_file = argv[i]
+    else:
+        new_argv.append(argv[i])
     i += 1
 
+include_line = ' '.join(new_argv) + ' '
 
 o_file = output_file
 if not (o_file.endswith('.o')):
@@ -31,13 +36,13 @@ if not (o_file.endswith('.o')):
 #print(input_file)
 #print(output_file)
 #print(o_file)
-
+    
 # .c -> .o
 if (input_file.endswith('.c')):
-    os.system('clang -c -O0 -emit-llvm ' + input_file + ' -o - | /Users/tarunraj/Projects/build/bin/opt -stats -load /Users/tarunraj/Projects/build/lib/PREviaLCM.dylib -reassociate -newgvn | /Users/tarunraj/Projects/build/bin/llc -filetype=obj > ' + output_file)
+    os.system('clang -c -O0 -emit-llvm ' + include_line + ' ' + input_file + ' -o - | /Users/tarunraj/Projects/build/bin/opt -stats -load /Users/tarunraj/Projects/build/lib/PREviaLCM.dylib -reassociate -newgvn | /Users/tarunraj/Projects/build/bin/llc -filetype=obj > ' + o_file)
     if not (output_file.endswith('.o')):
-        os.system('clang ' + o_file + ' -o ' + output_file)
+        os.system('clang ' + include_line + ' ' + o_file + ' -o ' + output_file)
 elif (len(output_file) > 0):  # .o -> exe
-    os.system('clang ' + input_file + ' -o ' + output_file)
-else: # else
-    os.system('clang ' + argv)
+    os.system('clang ' + include_line + ' ' + input_file + ' -o ' + output_file)
+else: # else 
+    os.system('clang ' + ' '.join(argv))
