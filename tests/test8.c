d785a17bed8529328c2e3f1a56584717e22efb11
Yiyi Wang <shd101wyy@sina.com>
2017-05-12 23:01:25 -0500

    modified test8.c to demonstrate loop invariants are partially redundant

--- tests/test8.c
+++ tests/test8.c
@@ -1,11 +1,13 @@
-
+// loop invariants are paritially redundant.
 int test(int a, int b) {
   int i = 0;
+  int t = 0;
   while (i < 10) {
     i += 1;
-    a += 1;
-    int t = a + b;
+    // a += 1;
+    t = a + b;
   }
+  return t;
 }
 
 int main() {
